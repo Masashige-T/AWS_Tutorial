0698cdaf7f40d28c7143401202d67c4238df80a0
Masashige-T <m.t0719@outlook.jp>
2020-08-25 19:55:26 +0900

    update: Cognitoと連携

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
-    cognito: {
-        userPoolId: 'ap-northeast-1_qJ93GvPOx', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '14uikkdrl3bbpjpdcbsae1thh7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'ap-northeast-1' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: 'https://5gguq5iaol.execute-api.ap-northeast-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
-};
+  cognito: {
+    userPoolId: "ap-northeast-1_YeAl7l7BQ", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "2elcfctcdv1t77ctgk73dpq3kt", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "ap-northeast-1", // e.g. us-east-2
+  },
+  api: {
+    invokeUrl:
+      "https://5gguq5iaol.execute-api.ap-northeast-1.amazonaws.com/prod", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  },
+}
